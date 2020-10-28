72d15bd4bc7d1e01a95dbd4f1a14204a6d56ab12
root <root@node-1.europe-west1-b.c.fit-accumulator-292319.internal>
2020-10-28 09:03:30 +0000

    Adaptació scripts LeChuck13

--- run-master.sh
+++ run-master.sh
@@ -3,7 +3,7 @@ curl https://get.docker.com | sudo bash
 
 mkdir -p /srv/docker
 #	Configurar NFS
-echo 'maquina-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+echo 'node-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-kernel-server
 echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 systemctl start nfs-kernel-server
@@ -14,7 +14,7 @@ docker swarm join-token manager|grep join  > /srv/docker/join.sh
 chmod +x !$
 
 cd /srv/docker
-git clone https://github.com/kpeiruza/swarm-cluster-example
+git clone https://github.com/LeChuck13/swarm-cluster-example
 
 docker network create proxy -d overlay
 docker network create portainer_agent -d overlay

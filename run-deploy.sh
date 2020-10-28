676f88b8269c13235846105f85644122c322515c
root <root@node-1.europe-west1-b.c.fit-accumulator-292319.internal>
2020-10-28 10:08:38 +0000

    Canvi variables IPs, passar per paràmetre

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IP1=$(getent hosts node-1 | awk '{print $1}')
-IP2=$(getent hosts node-2 | awk '{print $1}')
-IP3=$(getent hosts node-3 | awk '{print $1}')
+IP1=$1
+IP2=$2
+IP3=$3
 export IP1 IP2 IP3
 export REMOTE_MOUNT="/srv/docker/data"
 
